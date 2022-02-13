761560ac4a30ed6524d93f58724711a36267d706
Justin Rackley <justin.rackley@dal.ca>
2022-02-13 14:55:57 -0400

    updated main.tf to remove ec2 instance and add divider comment

--- main.tf
+++ main.tf
@@ -14,16 +14,9 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
 
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
-}
-
-# Call modules that are in modules folder
+# --------------------------------------------------
+# :: Module calls
 module "patient_module" {
   source = "./modules/patient"
 }
