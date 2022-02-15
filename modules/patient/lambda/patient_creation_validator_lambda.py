38b14889cd08d929852c0db8a467a6bfda55e419
Justin Rackley <justin.rackley@dal.ca>
2022-02-14 20:20:17 -0400

    updated python lambda code for patient validator

--- modules/patient/lambda/patient_creation_validator_lambda.py
+++ modules/patient/lambda/patient_creation_validator_lambda.py
@@ -3,7 +3,7 @@ import logging
 import json
 import os
 
-__PATIENT_CREATOR_LAMBDA_ARN = os.getenv(PATIENT_CREATOR_LAMBDA_ARN)
+__PATIENT_CREATOR_LAMBDA_ARN = os.getenv('PATIENT_CREATOR_LAMBDA_ARN')
 
 lambda_client = boto3.client('lambda')
 
@@ -15,7 +15,7 @@ def lambda_handler(event, context):
     __logger.info("This lambda was invoked with event: %s", event)
 
     response = lambda_client.invoke(
-        FunctionName="__PATIENT_CREATOR_LAMBDA_ARN",
+        FunctionName=__PATIENT_CREATOR_LAMBDA_ARN,
         InvocationType="RequestResponse",
         Payload=json.dumps(event['key1'])
     )
