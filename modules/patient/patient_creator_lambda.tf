f04397d78616c644e16ab0c810b7a0a20aa347f5
Justin Rackley <justin.rackley@dal.ca>
2022-02-14 20:36:06 -0400

    added enviroment variable for dynamodb table

--- modules/patient/patient_creator_lambda.tf
+++ modules/patient/patient_creator_lambda.tf
@@ -12,6 +12,12 @@ resource "aws_lambda_function" "the_patient_creator_lambda_function" {
   source_code_hash = data.archive_file.the_patient_creator_lambda_zip.output_base64sha256
   publish          = true
 
+  environment {
+    variables = {
+      PATIENT_TABLE_NAME = aws_dynamodb_table.the_patient_table.name
+    }
+  }
+
   depends_on = [aws_cloudwatch_log_group.the_patient_creator_lambda_cloudwatch_group]
 }
 
