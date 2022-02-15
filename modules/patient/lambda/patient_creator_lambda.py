1c1a2020c70e06afaa288a0cfd5cff21ba4ecfd8
Justin Rackley <justin.rackley@dal.ca>
2022-02-14 20:36:26 -0400

    try to add something to dynamdb table with python code

--- modules/patient/lambda/patient_creator_lambda.py
+++ modules/patient/lambda/patient_creator_lambda.py
@@ -1,12 +1,28 @@
 import boto3
 import logging
+import os
 
-lambda_client = boto3.client('lambda')
+__DYNAMO_DB_TABLE_NAME = os.getenv('PATIENT_TABLE_NAME')
+
+dynamodb = boto3.client('dynamodb')
 
 __logger = logging.getLogger()
 __logger.setLevel(logging.INFO)
 
+
 def lambda_handler(event, context):
     __logger.info("This lambda was invoked with event: %s", event)
 
-    return "Validator return value"
+    response = dynamodb.put_item(
+        TableName=__DYNAMO_DB_TABLE_NAME,
+        Item={
+            'patient_id': '345654',
+            'sort_key': 'movie',
+            'info': {
+                'plot': "plot",
+                'rating': "rating"
+            }
+        }
+    )
+
+    return response
