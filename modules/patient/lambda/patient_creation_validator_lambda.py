e3db37c75139cc72e2b80e0eb28351e3bf9ec705
Justin Rackley <justin.rackley@dal.ca>
2022-02-20 15:35:45 -0400

    removed whitespace

--- modules/patient/lambda/patient_creation_validator_lambda.py
+++ modules/patient/lambda/patient_creation_validator_lambda.py
@@ -18,19 +18,19 @@ def lambda_handler(event, context):
     body = json.loads(event['body'])
 
     # Make sure all patient data is included in the request
-    if "first_name" not in  body:
+    if "first_name" not in body:
         __bad_request.update({
             "body": "The patient could not be added as first_name was not defined"
         })
         return __bad_request
 
-    if "last_name" not in  body:
+    if "last_name" not in body:
         __bad_request.update({
             "body": "The patient could not be added as last_name was not defined"
         })
         return __bad_request
 
-    if "date_of_birth" not in  body:
+    if "date_of_birth" not in body:
         __bad_request.update({
             "body": "The patient could not be added as date_of_birth was not defined"
         })
