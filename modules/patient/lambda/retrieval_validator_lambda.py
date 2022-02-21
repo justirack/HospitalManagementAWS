2fc314e50271309b74c47054d179e298530ccf42
Justin Rackley <justin.rackley@dal.ca>
2022-02-20 20:42:12 -0400

    added some base lambda code for retrieval lambdas

--- modules/patient/lambda/retrieval_validator_lambda.py
+++ modules/patient/lambda/retrieval_validator_lambda.py
@@ -1,2 +1,15 @@
+import boto3
+import json
+import os
+import logging
+
+__PATIENT_RETRIEVER_LAMBDA_ARN = os.getenv('PATIENT_RETRIEVER_LAMBDA_ARN')
+
+lambda_client = boto3.client('lambda')
+
+__logger = logging.getLogger()
+__logger.setLevel(logging.INFO)
+
+
 def lambda_handler(event, context):
     pass
--- modules/patient/lambda/retriever_lambda.py
+++ modules/patient/lambda/retriever_lambda.py
@@ -1,2 +1,15 @@
+import boto3
+import json
+import os
+import logging
+
+__DYNAMO_DB_TABLE_NAME = os.getenv('PATIENT_TABLE_NAME')
+
+dynamodb = boto3.client('dynamodb')
+
+__logger = logging.getLogger()
+__logger.setLevel(logging.INFO)
+
+
 def lambda_handler(event, context):
     pass
