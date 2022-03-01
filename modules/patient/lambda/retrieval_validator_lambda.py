ba3c286e1c3311a1f8b2a2d7e26521ddf069c88d
Justin Rackley <justin.rackley@dal.ca>
2022-02-28 20:50:36 -0400

    added initial code to make call to retriever lambda

--- modules/patient/lambda/retrieval_validator_lambda.py
+++ modules/patient/lambda/retrieval_validator_lambda.py
@@ -10,8 +10,31 @@ lambda_client = boto3.client('lambda')
 __logger = logging.getLogger()
 __logger.setLevel(logging.INFO)
 
+__bad_request = {"statusCode": 400, "headers": {'Content-Type': 'text/html; charset=utf-8'}, "body": ""}
+
 
 def lambda_handler(event, context):
+    __logger.info(f'This lambda was invoked with event: {event}')
+
+    # Make sure it will be possible to search the table
+    if 'id' not in event['queryStringParameters']:
+        __logger.error(f'A patients id must be passed to retrieve it from the database')
+        __bad_request.update({
+            "body": "A patients id must be passed to retrieve it from the database"
+        })
+        return __bad_request
+
+    # Invoke the lambda that will search for the patient
+    __logger.info(f'Retrieving patient with id {event["queryStringParameters"]["id"]}')
+    response = lambda_client.invoke(
+        FunctionName=__PATIENT_RETRIEVER_LAMBDA_ARN,
+        InvocationType="RequestResponse",
+        Payload=json.dumps(event['queryStringParameters']['id'])
+    )
+
+    response_payload = json.load(response["Payload"])
+    __logger.info(f'Received response: {response_payload}')
+
     return {
         "statusCode": 200,
         "headers": {
