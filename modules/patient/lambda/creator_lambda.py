644ed9ac7b5a2e970c9642f647cb2173cb872600
Justin Rackley <justin.rackley@dal.ca>
2022-07-18 22:46:00 -0300

    finished updated python logic for adding a patient to dynamoDB

--- modules/patient/lambda/creator_lambda.py
+++ modules/patient/lambda/creator_lambda.py
@@ -1,10 +1,10 @@
+import os
+import json
+import uuid
 import boto3
 import logging
-import os
-import uuid
 
-__DYNAMO_DB_TABLE_NAME = os.getenv('PATIENT_TABLE_NAME')
-
+__dynamodb_table_name = os.getenv('PATIENT_TABLE_NAME')
 __dynamodb = boto3.client('dynamodb')
 
 __logger = logging.getLogger()
@@ -12,55 +12,35 @@ __logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event, context):
+    """
+    The event handler/entrypoint for this lambda.
+
+    This lambda will add a patient to the dynamoDB table.
+    It will use a UUID as the source id and a combination of first name,
+    last name and date of birth as the sort key.
+    """
     __logger.info(f"This lambda was invoked with event: {event}")
 
-    # patient_id = str(uuid.uuid4())
-    # sort_key = event['first_name'][0] + event['last_name'][0] + event['date_of_birth']
-    #
-    # response = __dynamodb.put_item(
-    #     TableName=__DYNAMO_DB_TABLE_NAME,
-    #     Item={
-    #         "patient_id": {'S': patient_id},
-    #         'sort_key': {'S': sort_key},
-    #         'first_name': {'S': event['first_name']},
-    #         'last_name': {'S': event['last_name']},
-    #         "date_of_birth": {'S': event['date_of_birth']}
-    #     }
-    # )
-    # __logger.info(f"Received response: {response}")
-    #
-    # # Add the patient id to the response if the add was successful
-    # if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-    #     response.update({
-    #         'patient_id': patient_id
-    #     })
-    #
-    # return response
+    for record in event['Records']:
+        body = json.loads(record['body'])
+        __logger.info(body)
 
+        patient_id = str(uuid.uuid4())
+        sort_key = body['first_name'][0] + body['last_name'][0] + body['date_of_birth']
 
+        response = __dynamodb.put_item(
+            TableName=__dynamodb_table_name,
+            Item={
+                "patient_id": {'S': patient_id},
+                'sort_key': {'S': sort_key},
+                'first_name': {'S': body['first_name']},
+                'last_name': {'S': body['last_name']},
+                "date_of_birth": {'S': body['date_of_birth']}
+            }
+        )
+        __logger.info(f"Received response: {response}")
 
-
-# Received record:
-# {
-#     "Records": [
-#         {
-#             "messageId": "7110e211-c62f-4aff-8044-9b7e75b03f21",
-#             "receiptHandle": "AQEByWazHXsjWO8XLb0zVwVXvOsVBkhTFQEFmCNgdQm5dWADu2PevG0qVhNZ4+Y+mb2kJn/4WqPZdv3eiJkY3z/NYeEy+qtzAgHZB5RwW5Lx3ZSmACDM0mDEyS08C0HD2fw1c+4aMoIOdgqfY3SZvwwRFqujoVsCclfyLBZCO9QQQjUGf0QofMv2Uav+EmCkhxGIivb2HcxLXnAtYMnfqzW0vSpLJrloWtRzWKBL+GfXqIE6nyBtG/H75QHahhni79dY1fr8ziRZMc2NZz/kntoY3MFiT5W52deg0Ok4Ejbv0BgvqQ497LCoJlzUjfHA+OaAUG+cg8g1qGzvlIXqRGcoGXCaaqjk/+IU526qgxmbDkrK/BYOdXo41ePep8xPI2P3ry5zdXzgRTNt06f0KnCa/1BMbl3bZ/J5eqr3NNYiEzc=",
-#             "body": "{\"first_name\": \"Justin\", \"last_name\": \"Rackley\", \"date_of_birth\": \"2002-02-09\"}",
-#             "attributes": {
-#                 "ApproximateReceiveCount": "1",
-#                 "SentTimestamp": "1657283547731",
-#                 "SenderId": "AROASEIXTSLSZY3HRM6KE:patient-patient_validator",
-#                 "ApproximateFirstReceiveTimestamp": "1657283547743"
-#             },
-#             "messageAttributes": {
-#
-#             },
-#             "md5OfMessageAttributes": "None",
-#             "md5OfBody": "7ed594e5565f27a53d71869d5fe701a7",
-#             "eventSource": "aws:sqs",
-#             "eventSourceARN": "arn:aws:sqs:us-west-2:146615276261:patient-create_patient_queue",
-#             "awsRegion": "us-west-2"
-#         }
-#     ]
-# }
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            __logger.info("Patient was successfully added to database")
+        else:
+            __logger.warning(f"Something went wrong adding the patient. Received status: {response['ResponseMetadata']['HTTPStatusCode']}.")
--- modules/patient/lambda/patient_validator.py
+++ modules/patient/lambda/patient_validator.py
@@ -6,19 +6,25 @@ import logging
 __logger = logging.getLogger()
 __logger.setLevel(logging.INFO)
 
-__sqs = boto3.client('sqs')
-
 __base_path = '/v1/patient/'
 
-__CREATE_PATIENT_QUEUE_URL = os.getenv('CREATE_PATIENT_QUEUE_URL')
+__sqs = boto3.client('sqs')
+__create_patient_queue_url = os.getenv('CREATE_PATIENT_QUEUE_URL')
 
 
 def lambda_handler(event, context):
+    """
+    The event handler/entrypoint for this lambda.
+
+    The lambda will validate the contents of the event
+    based on which endpoint it was invoked from.
+    """
     __logger.info(f'Lambda was invoked with: {event}')
 
     path = event['path']
     body = event['body']
 
+    # Will have this logic for each endpoint
     if path == __base_path + 'add':
         __logger.info('Invoked by the add endpoint. Trying to add a patient to the database.')
 
@@ -26,14 +32,20 @@ def lambda_handler(event, context):
             dict_contains_item(body, 'first_name')
             dict_contains_item(body, 'last_name')
             dict_contains_item(body, 'date_of_birth')
-        except AttributeError as error:
-            __logger.error(f'Error was raised: {error}')
+        except AssertionError as error:
+            __logger.error(f'Error was raised validating contents of event: {error}')
+            return format_return_message(400, str(error))
 
-        response = send_message_to_queue(__CREATE_PATIENT_QUEUE_URL, event['body'])
+        response = send_message_to_queue(__create_patient_queue_url, event['body'])
 
         __logger.info(f'Response: {response}')
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            return format_return_message(200, "The patient will be added to the database")
+        else:
+            return format_return_message(500, "Something went wrong. Please try again later.")
     elif path == __base_path + 'get':
         __logger.info('Invoked by the get endpoint. Trying to get a patient from the database')
+
 
     return {
         "statusCode": 200,
@@ -47,19 +59,38 @@ def lambda_handler(event, context):
 
 
 def dict_contains_item(dict_to_check, item):
-    # Check if the item is in the dict, throw an error if not
-    # Check GhostWriter code for example (release_consumer I think)
+    """
+    Checks if dict_to_check contains item
+    Will raise an AssertionError if dict_to_check does not contain the item
 
-    # This isn't the correct syntax
+    :param dict_to_check: A dictionary to check for an item in.
+    :param item: The item to check the dictionary for.
+    :return: True, or an error will be raised.
+    """
     if item not in dict_to_check:
-        raise AttributeError
+        raise AssertionError(f'The request must contain {item} in order to process successfully.')
 
 
 def send_message_to_queue(queue_url, body):
+    """
+    Sends a message to the queue with url queue_url
+
+    :param queue_url: The url of the queue to send the message to.
+    :param body: The body of the message.
+    :return: The response from SQS.
+    """
     response = __sqs.send_message(
         QueueUrl=queue_url,
-        MessageBody=json.dumps(body)
+        MessageBody=body
     )
 
     __logger.info(f'Response from SQS: {response}')
     return response
+
+
+def format_return_message(status, body):
+    return {
+        "statusCode": status,
+        "headers": {"Content-Type": "text/html; charset=utf-8"},
+        "body": body}
+
