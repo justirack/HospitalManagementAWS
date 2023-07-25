ddb1f38a3acf7b73dc2c7c352d2be0ef352541a6
Justin Rackley <justin.rackley@dal.ca>
2023-07-25 13:28:46 -0300

    Moved add and get user into separate functions to avoid a massive if elif ladder

--- modules/User/lambda/user_validation.py
+++ modules/User/lambda/user_validation.py
@@ -35,80 +35,15 @@ def lambda_handler(event: dict, context: dict):
 
     # Handle a request to add a patient to the database
     if path == __base_path + 'add':
-        __logger.info(f'Invoked by the add endpoint. Validating information in request.')
-
-        # Use the dict_contains_item function to make sure all required information is present in the request
-        try:
-            dict_contains_item(body, "first_name")
-            dict_contains_item(body, 'last_name')
-            dict_contains_item(body, 'date_of_birth')
-            dict_contains_item(body, 'phone_number')
-            dict_contains_item(body, 'user_type')
-
-            validate_phone_number(body['phone_number'])
-            datetime.date.fromisoformat(body['date_of_birth'])
-        # If something is missing an error will be raised, catch it and return an error message to the user
-        except AssertionError as error:
-            __logger.error(f'An error was raised validating the contents of the request: {error}')
-            return format_return_message(400, str(error))
-        except ValueError as error:
-            __logger.error(str(error))
-            return format_return_message(400, str(error))
-
-        # Invoke the creation lambda function. This will create a new record in the database.
-        __logger.info(f'All required information is present. Adding patient to database.')
-        response = __lambda.invoke(
-            FunctionName=__user_creation_lambda_arn,
-            InvocationType="RequestResponse",
-            Payload=event['body']
-        )
-        __logger.info(f'Creation lambda returned: {response}')
-
-        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-            # Parse the user id from the response, so it can be returned to the user
-            payload = response['Payload']
-            payload_body = json.loads(payload.read())
-            user_id = payload_body["body"]["user_id"]
-
-            __logger.info(f'User was successfully added to the database. Their user ID is: {user_id}')
-            return format_return_message(200, f'User was successfully added to the database. Their user ID is: {user_id}')
-        # If the response code was not 200 something went wrong in the other lambda function,
-        # return an error message to the user
-        else:
-            return format_return_message(response['ResponseMetadata']['HTTPStatusCode'], "Something went wrong.")
+        return add_user(event, body)
 
     # Handle a request to get a patient from the database
     elif path == __base_path + 'get':
-        __logger.info(f'Invoked by the get endpoint. Validating request.')
-
-        try:
-            if event['queryStringParameters'] is None:
-                raise AssertionError(f'The user id must be passed as a query string parameter.')
-        except AssertionError as error:
-            __logger.error(f'An error was raised validating the request: {error}')
-            return format_return_message(400, str(error))
-
-        response = __lambda.invoke(
-            FunctionName=__user_retrieval_lambda_arn,
-            InvocationType="RequestResponse",
-            Payload=json.dumps(event['queryStringParameters']['id'])
-        )
-        __logger.info(f'Retrieval lambda returned: {response}')
-        users = json.load(response['Payload'])
-
-        if response['StatusCode'] == 200 and users is not None:
-            __logger.info(f'Retrieved {len(users)} user(s) from the database. Returning them to the user.')
-            return format_return_message(200, json.dumps(users))
-        elif response['StatusCode'] == 200 and users is None:
-            __logger.info(f'Did not find any users in the database')
-            return format_return_message(200, "No users found.")
-        else:
-            __logger.info(f'Something went wrong. Try again later.')
-            return format_return_message(response['StatusCode'], f'Something went wrong. Try again later.')
+        return get_user(event)
 
     # Handle a request to update a patient in the database
     elif path == __base_path + 'update':
-        return format_return_message(200, "Placeholder return until functionality for other lambdas is implemented.")
+        return update_user(event, body)
 
     # Handle a request to remove a patient from the database
     elif path == __base_path + 'delete':
@@ -118,6 +53,91 @@ def lambda_handler(event: dict, context: dict):
     else:
         __logger.error(f'Invoked by an unknown endpoint: {path}')
         return format_return_message(500, 'The validation lambda was invoked from an unknown endpoint.')
+
+
+def add_user(event: dict, body: dict):
+    __logger.info(f'Invoked by the add endpoint. Validating information in request.')
+
+    # Use the dict_contains_item function to make sure all required information is present in the request
+    try:
+        dict_contains_item(body, "first_name")
+        dict_contains_item(body, 'last_name')
+        dict_contains_item(body, 'date_of_birth')
+        dict_contains_item(body, 'phone_number')
+        dict_contains_item(body, 'user_type')
+
+        validate_phone_number(body['phone_number'])
+        datetime.date.fromisoformat(body['date_of_birth'])
+    # If something is missing an error will be raised, catch it and return an error message to the user
+    except AssertionError as error:
+        __logger.error(f'An error was raised validating the contents of the request: {error}')
+        return format_return_message(400, str(error))
+    except ValueError as error:
+        __logger.error(str(error))
+        return format_return_message(400, str(error))
+
+    # Invoke the creation lambda function. This will create a new record in the database.
+    __logger.info(f'All required information is present. Adding patient to database.')
+    response = __lambda.invoke(
+        FunctionName=__user_creation_lambda_arn,
+        InvocationType="RequestResponse",
+        Payload=event['body']
+    )
+    __logger.info(f'Creation lambda returned: {response}')
+
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        # Parse the user id from the response, so it can be returned to the user
+        payload = response['Payload']
+        payload_body = json.loads(payload.read())
+        user_id = payload_body["body"]["user_id"]
+
+        __logger.info(f'User was successfully added to the database. Their user ID is: {user_id}')
+        return format_return_message(200, f'User was successfully added to the database. Their user ID is: {user_id}')
+    # If the response code was not 200 something went wrong in the other lambda function,
+    # return an error message to the user
+    else:
+        return format_return_message(response['ResponseMetadata']['HTTPStatusCode'], "Something went wrong.")
+
+
+def get_user(event: dict):
+    __logger.info(f'Invoked by the get endpoint. Validating request.')
+
+    try:
+        if event['queryStringParameters'] is None:
+            raise AssertionError(f'The user id must be passed as a query string parameter.')
+    except AssertionError as error:
+        __logger.error(f'An error was raised validating the request: {error}')
+        return format_return_message(400, str(error))
+
+    response = __lambda.invoke(
+        FunctionName=__user_retrieval_lambda_arn,
+        InvocationType="RequestResponse",
+        Payload=json.dumps(event['queryStringParameters']['id'])
+    )
+    __logger.info(f'Retrieval lambda returned: {response}')
+    users = json.load(response['Payload'])
+
+    if response['StatusCode'] == 200 and users is not None:
+        __logger.info(f'Retrieved {len(users)} user(s) from the database. Returning them to the user.')
+        return format_return_message(200, json.dumps(users))
+    elif response['StatusCode'] == 200 and users is None:
+        __logger.info(f'Did not find any users in the database')
+        return format_return_message(200, "No users found.")
+    else:
+        __logger.info(f'Something went wrong. Try again later.')
+        return format_return_message(response['StatusCode'], f'Something went wrong. Try again later.')
+
+
+def update_user(event: dict, body:dict):
+    __logger.info(f'Invoked by the update endpoint. Validating request.')
+
+    try:
+        keys = body.keys()
+        __logger.info(f'Keys: {keys}')
+    except AssertionError as error:
+        pass
+
+    return format_return_message(200, "Placeholder return until functionality for other lambdas is implemented.")
 
 
 def dict_contains_item(check_dict: dict, item: str):
